9c0020e5715495c020ef5f28bbdb250e2ad584c7
roryc89 <roryc89@googlemail.com>
2020-02-24 22:45:52 +0700

    finishes should restart logic

--- monitor.js
+++ monitor.js
@@ -1,17 +1,18 @@
-var rp = require('request-promise');
+const rp = require('request-promise');
+const moment = require('moment')
 require('dotenv').config()
 
 
 const getLogs = async () => {
     const options = {
-        uri: 'https://papertrailapp.com/api/v1/events/search.json?limit=2000',
+        uri: 'https://papertrailapp.com/api/v1/events/search.json?q=heroku/router&limit=1000',
         headers: {
             'X-Papertrail-Token': process.env.PAPERTRAIL_TOKEN
         },
         json: true
     };
 
-    const {events} = await rp(options)
+    const { events } = await rp(options)
 
     return events
 
@@ -20,29 +21,42 @@ const getLogs = async () => {
 const getStatusCode = (log) => {
 
     try {
-        const parts = log.message.split(' status=')
-        return Number(parts[1].slice(0, 3))
-    }catch(e){
+        const pair = log.message.split(' status=')
+        return Number(pair[1].slice(0, 3))
+    } catch (e) {
         return null;
     }
 }
 
-const shouldRestart = ({ allowedTimeoutRatio, allowedErrorRatio }, logs) => {
+const shouldRestart = ({ allowedTimeoutRatio, allowedErrorRatio, minRequests, intervalMs, previousIncludedIntervals }, now, logs) => {
 
-    const routerLogs = logs.filter(l => l.program === 'heroku/router')
+    
+    const nowMoment = moment(now)
 
-    const statusCodes = routerLogs.map(getStatusCode).filter(sc => sc)
+    const recentLogs = logs.filter(l => {
 
+        const logMoment = moment(l.received_at)
+        
+        return nowMoment.diff(logMoment) < intervalMs * (previousIncludedIntervals + 1)
 
-    if(statusCodes.length < 100){ // not enough data to know if we should restart
-        return false
-    }
+    });
+
+    const statusCodes = recentLogs.map(getStatusCode).filter(sc => sc)    
 
     const errorRatio = statusCodes.filter(sc => sc >= 500).length / statusCodes.length
     const timeoutRatio = statusCodes.filter(sc => sc === 503).length / statusCodes.length
-    
+
+    console.log('nowMoment', nowMoment);
+    console.log('statusCodes.length', statusCodes.length);
+    console.log('errorRatio', errorRatio);
+    console.log('timeoutRatio', timeoutRatio);
+
+    if (statusCodes.length < minRequests) { // not enough data to know if we should restart
+        return false
+    }
+
     return errorRatio > allowedErrorRatio || timeoutRatio > allowedTimeoutRatio
-    
+
 };
 
 const restartApp = async () => {
@@ -52,21 +66,26 @@ const restartApp = async () => {
 
 const go = async () => {
 
-    try{
+    try {
         const config = require('./config.json')
+        const {intervalMs} = config;
 
         const logs = await getLogs()
-    
-        if (shouldRestart(config, logs)) {
+
+        const now = Date.now();
+
+        if (shouldRestart(config, now, logs)) {
             await restartApp()
         }
 
-        await new Promise(r => setTimeout(r, config.sleepMsBetweenCycles));
+        await new Promise(r => setTimeout(r, intervalMs));
 
-    }catch(err){
+        process.exit(0)
+
+    } catch (err) {
         console.error(err)
     }
-    
+
 }
 
 go()
